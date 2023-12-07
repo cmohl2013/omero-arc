68b0be7ed9aa9464ac107a91f6623b3d996d0c79
cmohl2013 <christoph.oliver.moehl@gmail.com>
2023-12-07 09:46:09 +0100

    modified CI

--- test/test_arc_tmp.py
+++ test/test_arc_tmp.py
@@ -2,5 +2,5 @@ from abstract_arc_test import AbstractArcTest
 
 
 class TestArcTmp(AbstractArcTest):
-    def test_arc_tmp_1(self, dataset_1):
+    def test_arc_tmp_1(self, dataset_1, dataset_czi_1):
         assert True
