96fed5f7c0c14364f6bfb66a9655068b94d4251f
cmohl2013 <christoph.oliver.moehl@gmail.com>
2024-12-12 12:29:08 +0100

    added init test

--- test/test_arc_transfer.py
+++ test/test_arc_transfer.py
@@ -3,19 +3,32 @@ import pytest
 
 
 class TestArcTransfer(AbstractArcTest):
-    # def test_pack_arc_fails_for_dataset_cli(self, dataset_1, tmp_path):
-    #     # only projects can be packed as arc
-    #     dataset_identifier = f"Dataset:{dataset_1.id}"
-    #     path_to_arc = tmp_path / "my_arc"
-    #     args = self.args + [
-    #         "pack",
-    #         "--plugin","arc",
-    #         dataset_identifier,
-    #         str(path_to_arc),
-    #     ]
 
-    #     with pytest.raises(AssertionError):
-    #         self.cli.invoke(args)
+    def test_init_cli(self, project_1, tmp_path):
+        project_identifier = f"Project:{project_1.getId()}"
+        path_to_arc = tmp_path / "my_arc"
+        args = self.args + [
+            "pack",
+            "--plugin","arc",
+            project_identifier,
+            str(path_to_arc),
+        ]
+        self.cli.invoke(args)
+
+
+    def test_pack_arc_fails_for_dataset_cli(self, dataset_1, tmp_path):
+        # only projects can be packed as arc
+        dataset_identifier = f"Dataset:{dataset_1.id}"
+        path_to_arc = tmp_path / "my_arc"
+        args = self.args + [
+            "pack",
+            "--plugin","arc",
+            dataset_identifier,
+            str(path_to_arc),
+        ]
+
+        with pytest.raises(AssertionError):
+            self.cli.invoke(args)
 
     def test_pack_arc(self, project_1, tmp_path):
         project_identifier = f"Project:{project_1.getId()}"
